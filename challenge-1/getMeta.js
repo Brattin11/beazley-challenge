adf6993ee4f239e94ea4cdedeb987b7d6bf0c236
Michael Brattin <michaelbrattin@Michaels-MBP.lan>
2025-05-14 10:31:02 -0500

    challenge 1 - allow specifying instance metadata url

--- challenge-1/getMeta.js
+++ challenge-1/getMeta.js
@@ -1,11 +1,26 @@
+/* 
+    getMeta.js
+
+    A script used to query metadata from a particular instance of a Google Cloud Compute Engine. If no specific property
+    is requested, return all.
+
+    args - 
+    0: Instance metadata url - Ex: http://metadata.google.internal/computeMetadata/v1/instance/disks/?recursive=true
+    1: (optional) Desired propery - Ex: 'deviceName'
+
+    Can be invoked via the command line: 
+    node getMeta.js http://metadata.google.internal/computeMetadata/v1/instance/disks/?recursive=true deviceName
+*/
 const args = process.argv.slice(2);
 
-const getMetaData = async () => {
+const getMetaData = async (url, desiredProperty) => {
+    if (!url) {
+        return "Please provide instance metadata URL as the first argument"
+    }
     try {
-        const response = await fetch("http://metadata.google.internal/computeMetadata/v1/instance/disks/?recursive=true", {headers: {'Metadata-Flavor': 'Google'}})
+        const response = await fetch(url, {headers: {'Metadata-Flavor': 'Google'}})
         const json = await response.json()
-        if (args.length > 0) {
-            const desiredProperty = args[0];
+        if (args.length > 1) {
             if (json[0].hasOwnProperty(desiredProperty)) {
                 return {[desiredProperty]: json[0][desiredProperty]}
             } else {
@@ -15,8 +30,10 @@ const getMetaData = async () => {
             return json[0]   
         }
     } catch (e) {
-        return e
+        return ["Looks like there was an error. Make sure you're passing the instance metadata URL as the first argument." ,e]
     }
 }
 
-getMetaData().then(res => console.log(res))
+[url, desiredProperty] = args;
+
+getMetaData(url, desiredProperty).then(res => console.log(res))
